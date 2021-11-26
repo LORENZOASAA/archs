fefa9dc86a20803bdba7f69c3a0cb6735e5586c3
LORENZOASAA <76909643+LORENZOASAA@users.noreply.github.com>
2021-11-26 18:56:29 +0100

    Create parte2.sh

---
+++ parte2.sh
@@ -0,0 +1,11 @@
+Uscita
+riavviare
+Elenco dispositivi iwctl 
+ip linkiwctl station wlan0 get-networks
+stazione iwctl scansione wlan0 
+iwctl station wlan0 connect SSID
+pacman -S alsa-utils
+pacman -S xorg-server xorg-xinit
+pacman -S networkmanager
+systemctl abilita NetworkManager
+systemctl disabilita dhcpcd
